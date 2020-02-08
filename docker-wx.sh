c9789e9586185c8653ee01788764fc4bfd5360fd
Alexound <alexound.login@gmail.com>
2020-02-08 21:35:01 +0400

    Modified Vagrantfile & added docker-wx.sh script.
    docker-wx.sh performs installation of Docker Community Edition.

--- docker-wx.sh
+++ docker-wx.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# First: general systemd update and upgrade.
 sudo yum update -y
+
+# Second: install and configure vim for administrative tasks.
 sudo yum install vim -y
 echo -e "\nset number\nset shiftwidth=4\nset tabstop=4" | sudo tee -a /etc/vimrc
 
+# Third: Install required dependencies for further docker installation.
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# Fourth: add docker repository.
 sudo yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Fifth: install docker community edition.
 sudo yum install docker-ce docker-ce-cli containerd.io -y
+
+# Sixth: add current user (vagrant) to the docker group (to get rid of sudo).
 sudo usermod -aG docker vagrant
+
+# Seventh: acrually start docker service.
 service docker start
