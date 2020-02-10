2cd6305e1c02a0e11cdfa1537cb253985fc8613e
Alexound <alexound.login@gmail.com>
2020-02-10 23:46:40 +0400

    Fixed database building script.

--- pgsql_srv/db_build.sql
+++ pgsql_srv/db_build.sql
@@ -1,2 +1,8 @@
 CREATE ROLE sample WITH LOGIN PASSWORD 'sample';
 CREATE DATABASE sample_db OWNER sample;
+
+\connect sample_db;
+
+CREATE TABLE t1 (s serial, c char(16));
+GRANT ALL ON t1 TO sample;
+GRANT ALL ON t1_s_seq TO sample;
